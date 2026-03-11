2247af0d9b5f42b2ee0cd61c027dd62164226520
Dustin O'Connor <dustin.oconnor@gmail.com>
2026-03-11 08:43:22 -0400

    Initial Commit

---
+++ MetalNode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalNode
+//
+//  Created by Dustin O'Connor on 3/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalNode/MetalNodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalNodeApp.swift
+//  MetalNode
+//
+//  Created by Dustin O'Connor on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MetalNodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
